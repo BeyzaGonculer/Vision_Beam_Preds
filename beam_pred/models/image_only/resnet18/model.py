919392549af6b474e99ed13e157e73475453f059
Beyza Göncüler <96770787+BeyzaGonculer@users.noreply.github.com>
2025-06-04 10:45:23 +0300

    Update model.py

--- beam_pred/models/image_only/resnet18/model.py
+++ beam_pred/models/image_only/resnet18/model.py
@@ -2,13 +2,10 @@ import torch.nn as nn
 import torchvision.models as models
 
 def get_model(num_classes=64):
-    """
-    Beam tahmini için ResNet18 modelini yükler.
-    Son katmanı beam index sayısına göre ayarlar.
-    """
+
     model = models.resnet18(pretrained=True)
 
-    # Son katmanı değiştir (fc → full-connected layer)
+    
     in_features = model.fc.in_features
     model.fc = nn.Linear(in_features, num_classes)
 
