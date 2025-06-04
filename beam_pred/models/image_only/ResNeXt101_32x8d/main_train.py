e2f42eafc1045b18e0dbb0903f9b7c6faa0113f3
Beyza Göncüler <96770787+BeyzaGonculer@users.noreply.github.com>
2025-06-04 10:36:06 +0300

    Update main_train.py

--- beam_pred/models/image_only/ResNeXt101_32x8d/main_train.py
+++ beam_pred/models/image_only/ResNeXt101_32x8d/main_train.py
@@ -7,13 +7,13 @@ import torchvision.transforms as transforms
 import torch
 import torch.optim as optim
 
-# Dönüşümler
+
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
-# Veri Seti
+
 dataset = BeamPredictionDataset(
     csv_path="../../../dataset/scenario5_dev_train.csv",
     root_dir="../../../dataset/unit1",
@@ -21,14 +21,14 @@ dataset = BeamPredictionDataset(
 )
 dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
 
-# Model
+
 model = get_model(num_classes=64)
 
-#  Optimizer & Cihaz
+
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Eğitimi başlat
+
 train_model(model, dataloader, optimizer, device, num_epochs=5)
 
 # Eğitilen modeli kaydet
