ed9e48916c37fd9d6f330ced484412a6b07743c0
Ayça Dolar <84194556+AycaDolar@users.noreply.github.com>
2025-06-04 11:10:35 +0300

    Update main_train.py

--- beam_pred/models/image_only/resnet50/main_train.py
+++ beam_pred/models/image_only/resnet50/main_train.py
@@ -7,13 +7,11 @@ import torchvision.transforms as transforms
 import torch
 import torch.optim as optim
 
-# 🔁 Dönüşümler
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
-# 📁 Veri Seti
 dataset = BeamPredictionDataset(
     csv_path="../../../dataset/scenario5_dev_train.csv",
     root_dir="../../../dataset/unit1",
@@ -21,16 +19,12 @@ dataset = BeamPredictionDataset(
 )
 dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
 
-# 🧠 Model
 model = get_model(num_classes=64)
 
-# ⚙️ Optimizer & Cihaz
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# 🚀 Eğitimi başlat
 train_model(model, dataloader, optimizer, device, num_epochs=5)
 
-# Eğitilen modeli kaydet
 torch.save(model.state_dict(), "resnet50_beam_model.pth")
 print("Model başarıyla kaydedildi: resnet50_beam_model.pth")
